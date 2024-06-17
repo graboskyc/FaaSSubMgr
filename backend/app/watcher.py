5e7d80dd8c1cf76c37a62d1cff7d5d9f85f5f6b6
Chris Grabosky <chris@grabosky.net>
2024-06-17 14:17:42 -0400

    store response

--- backend/app/watcher.py
+++ backend/app/watcher.py
@@ -59,8 +59,15 @@ def runWatch(id, conStr, db, col, pipeline, rt, wh):
                 #print("SUBSCRIBER CHANGE")
                 print(change)
                 response = requests.post(wh, json=json.loads(dumps(change)))
+                responseToInsert = ""
+
+                try:
+                    responseToInsert = json.loads(response.text)
+                except:
+                    responseToInsert = response.text
+                    
                 #print(response.text)
-                masterHandle.update_one({"_id":id}, {"$set": {"resumeToken": resume_token}})
+                masterHandle.update_one({"_id":id}, {"$set": {"resumeToken": resume_token, "webhookResponse": responseToInsert }})
                 #print("resume token updated")
     except pymongo.errors.PyMongoError:
         print("Error in watcher")
